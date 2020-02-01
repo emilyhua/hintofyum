a37cb469f9e5fccc4515cced54ad7c9d8a411f84
Sarah Rosenblatt <sgr19@pitt.edu>
2020-02-01 16:08:05 -0500

    added flask

---
+++ flask_yum.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import simplejson as json
+import requests
+from mainyum import find_from_ingredients, display_recipe_names, get_recipe
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def search_recipes():
+    if request.method == 'POST':
+        content = find_from_ingredients(request.form["restaurant_name"])
+        json_response = json.loads(content.text)
+        print(json_response)
+        return render_template("test.html", response=json_response) if json_response != [] else render_template(
+            "test.html", response="")
+    else:
+        return render_template("test.html")
+
+# @app.route('/recipe{id}', methods['GET', 'POST'])
+# def display_recipe(id):
+#     if request.method == 'POST':
+#             content = get_recipe(id)
+#             json_response = json.loads(content.text)
+#             print(json_response)
+#             return render_template("test.html", response=json_response) if json_response != [] else render_template(
+#                 "restaurant_list.html", response="")
+#         else:
+#             return render_template("test.html")
--- mainyum.py
+++ mainyum.py
@@ -11,14 +11,25 @@ def display_recipe_names(query):
 
 def find_from_ingredients(ingredient_list):
     #ranking = 2 means minimizing missing ingredients
-    recipe_list = api.search_recipes_by_ingredients(ingredients=ingredient_list, number = 10, ranking=2)
+    recipe_list = api.search_recipes_by_ingredients(ingredients=ingredient_list, number=10, ranking=2)
     return recipe_list
 
-def parse_recipe(recipe_list):
+def get_recipe(id):
+    recipe_info = api.get_recipe_information(id)
+    return recipe_info
 
-
-ingredients = input("Enter ingredients you would like to cook with: ")
-recipes = find_from_ingredients(ingredients)
-for recipe in recipes:
-    print(recipe)
-    print("\n")
+# info = get_recipe(1136928)
+# for thing in info:
+#     print(thing)
+#
+# ingredients = input("Enter ingredients you would like to cook with: ")
+# recipes = find_from_ingredients(ingredients)
+# max_recipes = 10
+# curr_recipes = 0
+# recipe_id = -1
+# for recipe in recipes:
+#     # if curr_recipes < 10:
+#     #     recipe_id = parse_recipe(recipe)
+#     #     curr_recipes++
+#     print(recipe)
+#     print("\n")
